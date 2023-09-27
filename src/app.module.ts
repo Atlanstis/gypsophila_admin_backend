456fd818a38f6b98168f8a376dfeb7272119ffa6
Gypsophila <futur_wxc@163.com>
2023-09-27 10:52:37 +0800

    feat: 增加 typeorm 相关配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PsnineModule } from './psnine/psnine.module';
+import { PsGame } from './entities/ps-game.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'j!Y6hTwR',
+      database: 'gypsophila',
+      synchronize: true,
+      logging: true,
+      entities: [PsGame],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+        timezone: 'Asia/Shanghai',
+      },
+    }),
+    PsnineModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
