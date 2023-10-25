6793b39a4621e4de8e2dc5df57a383e465580ff9
Gypsophila <futur_wxc@163.com>
2023-10-25 14:39:12 +0800

    feat: 统一返回格式

---
+++ src/core/index.ts
@@ -0,0 +1,2 @@
+export * from './filter';
+export * from './interceptor';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter, TransformInterceptor } from './core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 成功返回数据时，对内容进行包裹
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
+
 bootstrap();
