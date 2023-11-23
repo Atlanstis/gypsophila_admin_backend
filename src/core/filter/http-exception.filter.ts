2919e61048767faa72ea24e65f3f36ba3bf4d121
Gypsophila <futur_wxc@163.com>
2023-11-23 09:39:24 +0800

    feat: 修改 ValidationPipe  抛吃异常的 status

--- src/core/filter/http-exception.filter.ts
+++ src/core/filter/http-exception.filter.ts
@@ -1,4 +1,11 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  ArgumentsHost,
+  BadRequestException,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { UnauthorizedException } from 'src/core';
 import { ResponseCode, ResponseData } from 'src/typings';
 
@@ -14,20 +21,19 @@ export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp(); // 获取请求上下文
     const response = ctx.getResponse(); // 获取请求上下文中的 response对象
-    const status = exception.getStatus(); // 获取异常状态码
-    const resJson = exception.getResponse() as ExcepionResJson; // 获取异常内容
+    let status = exception.getStatus(); // 获取异常状态码
 
     let code = ResponseCode.Error;
     // 设置错误信息
-    let message = exception.message
-      ? exception.message
-      : `${status >= 500 ? 'Service Error' : 'Client Error'}`;
+    let message = exception.message;
 
-    if (status === HttpStatus.BAD_REQUEST) {
+    if (exception instanceof BadRequestException) {
+      const { message: exceptionMessage } = exception.getResponse() as ExcepionResJson;
       // 通过 ValidationPipe 进行参数校验错误时，会抛出 BadRequestException 异常，此处进行捕获，并转换报错信息
-      const { message: exceptionMessage } = resJson;
       if (exceptionMessage) {
         message = Array.isArray(exceptionMessage) ? exceptionMessage.join(';') : exceptionMessage;
+        // 修改状态码
+        status = HttpStatus.OK;
       }
     }
 
