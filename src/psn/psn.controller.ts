95c7ae9c3b815fb23b8172d168befdb76650c94e
Gypsophila <futur_wxc@163.com>
2024-01-30 11:03:22 +0800

    feat: psn 部分接口，权限修改

--- src/psn/psn.controller.ts
+++ src/psn/psn.controller.ts
@@ -5,43 +5,54 @@ import { Request } from 'express';
 import { JwtGuard, PermissionGuard, RequirePermission } from 'src/core';
 
 @Controller('psn')
-@UseGuards(JwtGuard, PermissionGuard)
-@RequirePermission('PsnProfileOperation')
+@UseGuards(JwtGuard)
 export class PsnController {
   constructor(private readonly psnService: PsnService) {}
 
   /** 获取 psn 用户信息 */
   @Get('/profile')
+  @UseGuards(PermissionGuard)
+  @RequirePermission('PsnProfileOperation')
   async getProfile(@Req() req: Request) {
     return await this.psnService.getProfile(req.user.id);
   }
 
   /** 绑定 psn 用户信息 */
   @Post('/profile/bind')
+  @UseGuards(PermissionGuard)
+  @RequirePermission('PsnProfileOperation')
   async profileBind(@Body() dto: PsnIdDto, @Req() req: Request) {
     return await this.psnService.profileBind(dto.psnId, req.user.id);
   }
 
   /** 获取可以同步的游戏列表 */
   @Post('/game/synchronizeable')
+  @UseGuards(PermissionGuard)
+  @RequirePermission('PsnProfileOperation')
   async getGameSynchronizeable(@Body() dto: PageDto, @Req() req: Request) {
     return await this.psnService.getGameSynchronizeable(req.user.id, dto.page);
   }
 
   /** 同步游戏 */
   @Post('/game/sync')
+  @UseGuards(PermissionGuard)
+  @RequirePermission('PsnProfileOperation')
   async gameSync(@Body() dto: PsnineGameDto, @Req() req: Request) {
     return await this.psnService.gameSync(req.user.id, dto.gameId);
   }
 
   /** 获取已经同步的游戏 */
   @Post('/game/synchronized')
+  @UseGuards(PermissionGuard)
+  @RequirePermission('PsnProfileOperation')
   async gameSynchronized(@Body() dto: PageDto, @Req() req: Request) {
     return await this.psnService.gameSynchronized(req.user.id, dto.page);
   }
 
   /** 收藏/取消收藏 游戏 */
   @Post('/game/favor')
+  @UseGuards(PermissionGuard)
+  @RequirePermission('PsnProfileOperation')
   async gameFavor(@Body() dto: PsnProfileGameDto, @Req() req: Request) {
     return await this.psnService.gameFavor(req.user.id, dto.ppgId);
   }
