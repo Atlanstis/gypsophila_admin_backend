ca1011a8f2fabaa6023848e848053be0e7a4cf21
Gypsophila <futur_wxc@163.com>
2024-05-20 11:01:59 +0800

    feat(定时任务): 增加通知过期检查

--- src/schedule-task/task/index.ts
+++ src/schedule-task/task/index.ts
@@ -1,4 +1,9 @@
 import { MHXY_ACCOUNT_GOLD_DAILY } from './mhxy-account-gold-daily';
 import { MHXY_GOLD_TRANSFER_TODO_NOTIFY } from './mhxy-gold-transfer-todo-notify';
+import { NOTICE_EXPIRE_CHECK } from './notice-expire-check';
 
-export const TASKS_LIST = [MHXY_ACCOUNT_GOLD_DAILY, MHXY_GOLD_TRANSFER_TODO_NOTIFY];
+export const TASKS_LIST = [
+  MHXY_ACCOUNT_GOLD_DAILY,
+  MHXY_GOLD_TRANSFER_TODO_NOTIFY,
+  NOTICE_EXPIRE_CHECK,
+];
