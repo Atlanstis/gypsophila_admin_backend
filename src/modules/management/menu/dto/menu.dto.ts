25c44d5be911750e2b536d9bd5c4eb1eb54cb77c
Gypsophila <futur_wxc@163.com>
2025-01-03 17:29:18 +0800

    feat(菜单管理): 增加排序字段

--- src/modules/management/menu/dto/menu.dto.ts
+++ src/modules/management/menu/dto/menu.dto.ts
@@ -1,33 +1,44 @@
 import { OmitType, PickType } from '@nestjs/mapped-types';
-import { Allow, IsEnum, IsNotEmpty, IsNumber, Length } from 'class-validator';
+import {
+  Allow,
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  Length,
+} from 'class-validator';
 import { MenuTypeEnum } from '../entity';
 import { MenuConst } from '../constants';
 import { Type } from 'class-transformer';
 
 export class MenuDto {
-  @IsNotEmpty({ message: '菜单 id 不能为空' })
   @IsNumber({}, { message: '菜单 id 格式错误' })
+  @IsNotEmpty({ message: '菜单 id 不能为空' })
   @Type(() => Number)
   id: number;
 
-  @IsNotEmpty({ message: '菜单名称不能为空' })
   @Length(MenuConst.nameMin, MenuConst.nameMax, {
     message: `菜单名称长度为 ${MenuConst.nameMin} - ${MenuConst.nameMax} 个字符`,
   })
+  @IsNotEmpty({ message: '菜单名称不能为空' })
   /** 菜单名称 */
   name: string;
 
-  @IsNotEmpty({ message: '菜单标识不能为空' })
   @Length(MenuConst.keyMin, MenuConst.keyMax, {
     message: `菜单标识长度为 ${MenuConst.keyMin} - ${MenuConst.keyMax} 个字符`,
   })
+  @IsNotEmpty({ message: '菜单标识不能为空' })
   /** 菜单标识 */
   key: string;
 
   /** 菜单类型 */
-  @IsNotEmpty({ message: '菜单类型不能为空' })
   @IsEnum(MenuTypeEnum, { message: '菜单类型错误' })
+  @IsNotEmpty({ message: '菜单类型不能为空' })
   type: MenuTypeEnum;
+
+  @IsInt({ message: '排序字段格式错误' })
+  @IsNotEmpty({ message: '排序字段不能为空' })
+  order: number;
 }
 
 export class MenuAddDto extends OmitType(MenuDto, ['id']) {
